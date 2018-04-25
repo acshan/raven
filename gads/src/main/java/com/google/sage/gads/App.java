c4fa08204ced23ed7dea5ba743be55e0a2c0d019
sage.ace <sage.ace@outlook.com>
2018-04-25 19:36:53 +0800

    springboot -> gae standard env

--- gads/src/main/java/com/google/sage/gads/App.java
+++ gads/src/main/java/com/google/sage/gads/App.java
@@ -3,12 +3,21 @@ package com.google.sage.gads;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class App {
-
     public static void main(String[] args){
         SpringApplication.run(App.class,args);
     }
+
+    public class ServletInitializer extends SpringBootServletInitializer {
+        @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+            return application.sources(App.class);
+        }
+    }
+
 }
 
